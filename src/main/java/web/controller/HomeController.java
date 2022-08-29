918e88330fe49c5b6df3a4a6237eb5d31576e09c
kiba013 <baurzhan0502@gmail.com>
2022-08-29 19:59:55 +0600

    fixed

--- src/main/java/web/controller/HomeController.java
+++ src/main/java/web/controller/HomeController.java
@@ -4,20 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarService;
+import web.service.CarServiceImp;
 
 
 @Controller
 public class HomeController {
     @GetMapping()
-    public String homePage() {
+    public String getMainPage() {
         return "index";
     }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count,
                           Model model) {
-        model.addAttribute("car", new CarService().getCarList(count));
+        model.addAttribute("car", new CarServiceImp().getCarList(count));
         return "car";
     }
 }
